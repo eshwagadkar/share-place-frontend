68ed85340c5bdf0875eb47078f54f3282339abe5
eshwagadkar <eshwagadkar007@gmail.com>
2025-11-03 17:06:49 +0530

    managing state in input component using useReducer

--- src/places/pages/NewPlace.jsx
+++ src/places/pages/NewPlace.jsx
@@ -3,7 +3,11 @@ import './NewPlace.css'
 
 export default function NewPlace() {
     return <form className='place-form'>
-        <Input element='input' type='text' label='Title' />
+        <Input element='input'
+               type='text'
+               label='Title'
+               validators={[]}
+               errorText='Please enter a valid title' />
     </form>
     
 }
--- src/shared/components/FormElements/Input.jsx
+++ src/shared/components/FormElements/Input.jsx
@@ -1,15 +1,52 @@
+import { useReducer } from 'react'
 import './Input.css'
 
-export default function Input({ element, id, label, type, placeholder, rows }) {
+const inputReducer = (state, action) => {
+
+    switch (action.type) {
+    
+        case 'CHANGE': 
+            return {
+                ...state,
+                value: action.payload,
+                isValid: true
+            }
+        default:
+            return state
+    }
+
+
+}
+
+export default function Input({ element, id, label, type, placeholder, rows, errorText }) {
+
+
+    const [inputState, dispatch] = useReducer(inputReducer, { value: '', isValid: false })
+
+
+    function changeHandler (event) {
+        dispatch({ type: 'CHANGE', payload: event.target.value })
+    }
     
     const dynamicElement = element === 'input' ? 
-    <input id={id} type={type} placeholder={placeholder} /> :
-     <textarea id={id} rows={rows || 3}/>
+    <input id={id}
+           type={type}
+           placeholder={placeholder} 
+           onChange={changeHandler} 
+           value={inputState.value}
+
+    /> :
+    <textarea id={id} 
+              rows={rows || 3} 
+              onChange={changeHandler}
+              value={inputState.value}
+    />
     
     return (
-        <div className={`form-control`}>
+        <div className={`form-control ${!inputState.isValid && 'form-control--invalid'}`}>
             <label htmlFor={id}>{label}</label>
             { dynamicElement }
+            {!inputState.isValid && <p>{errorText}</p>}
         </div>
     )
 }
