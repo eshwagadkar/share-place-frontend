5f058e1dee07f9937f6ab6595f78937b9b25ee64
eshwagadkar <eshwagadkar007@gmail.com>
2025-11-06 18:36:32 +0530

    update place page and route

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,23 @@ import RootLayout from './shared/pages/Root'
 import Users from './user/pages/Users'
 import NewPlace from './places/pages/NewPlace'
 import UserPlaces from './places/pages/UserPlaces'
+import UpdatePlace from './places/pages/UpdatePlace'
+
 function App() {
 
   const router = createBrowserRouter([
     { path: '/', element: <RootLayout />,
       children: [
         { index: true, element: <Users /> },
-        { path: 'places', element: <NewPlace /> },
-        { path: ':uid/places', element: <UserPlaces /> }
+        { path: ':uid/places', element: <UserPlaces /> },
+        { path: 'places/new', element: <NewPlace /> },
+        { path: 'places/:placeId', element: <UpdatePlace /> }
       ] 
     },
   ])
 
   return (
-    <RouterProvider router={router}>
-      
-    </RouterProvider>
+    <RouterProvider router={router} />
   )
 }
 
