a9843b5b6758b45444b911734787d623ee9d0d82
eshwagadkar <eshwagadkar007@gmail.com>
2025-11-12 22:21:08 +0530

    leverage useHttp in users page component

--- src/shared/hooks/http-hooks.js
+++ src/shared/hooks/http-hooks.js
@@ -52,6 +52,6 @@ export default function useHttpClient() {
         }
     },[])
 
-    return { isLoading, error, sendRequest, clearError}
+    return {isLoading, error, sendRequest, clearError}
 
 }
--- src/user/pages/Users.jsx
+++ src/user/pages/Users.jsx
@@ -2,47 +2,28 @@ import { useEffect, useState } from 'react'
 import LoadingSpinner from '../../shared/components/UI/LoadingSpinner'
 import ErrorModal from '../../shared/components/UI/ErrorModal'
 import UsersList from '../components/UsersList'
+import useHttpClient from '../../shared/hooks/http-hooks'
 
 export default function Users() {
 
-    const [isLoading, setIsLoading] = useState(false)
-    const [error, setError] = useState()
+    const {isLoading, error, sendRequest, clearError } = useHttpClient()
     const [loadedUsers, setLoadedUsers] = useState()
 
     useEffect(() => {
-
         async function fetchUsers() {
-            setIsLoading(true)
-            
-                try{
-                    const response = await fetch('http://localhost:4001/api/v1/users')
-                    
-                    const responseData = await response.json()
-                    
-                    if(!response.ok){
-                        throw new Error(responseData.message)
-                    }
-
-                    setLoadedUsers(responseData.users)
-                    
-                } catch(error) {
-                    setError(error.message)
-                }
-
-            setIsLoading(false)
-
-
+            try{
+                const responseData = await sendRequest('http://localhost:4001/api/v1/users')
+                setLoadedUsers(responseData.users)
+            } catch(error) { }
         }
         fetchUsers()
-    },[])
-
-    const errorHandler = () => setError(null)
+    },[sendRequest])
 
     return (
         <>  
-            <ErrorModal onClear={errorHandler} error={error}/>
-            {isLoading && <div className="center"><LoadingSpinner /></div>}
-            {!isLoading && loadedUsers && <UsersList items={loadedUsers}/>}
+         <ErrorModal onClear={clearError} error={error}/>
+         {isLoading && <div className="center"><LoadingSpinner /></div>}
+         {!isLoading && loadedUsers && <UsersList items={loadedUsers}/>}
         </>
     )
 }
