b8a6b32a0231df77741cf18583f430a00d74c3de
eshwagadkar <eshwagadkar007@gmail.com>
2025-11-13 13:23:05 +0530

    leverage the useHttpClient hook to make a post request to create a new place

--- src/places/pages/NewPlace.jsx
+++ src/places/pages/NewPlace.jsx
@@ -1,12 +1,19 @@
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 import Input from '../../shared/components/FormElements/Input'
 import Button from '../../shared/components/FormElements/Button'
 import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/utils/validators'
 import { useForm } from '../../shared/hooks/form-hooks'
+import useHttpClient from '../../shared/hooks/http-hooks'
+import ErrorModal from '../../shared/components/UI/ErrorModal'
+import LoadingSpinner from '../../shared/components/UI/LoadingSpinner'
 
 import './PlaceForm.css'
 
 export default function NewPlace() {
-
+    const navigate = useNavigate()
+    const userId = useSelector(state => state.auth.userId)
+    const { isLoading, error, sendRequest, clearError } = useHttpClient()
     const [inputHandler, formState] = useForm({
                 title: {
                     value: '',
@@ -22,14 +29,31 @@ export default function NewPlace() {
                 }, 
             }, false)
 
-   
 
-    const placeSubmitHandler = event => {
+    const placeSubmitHandler = async event => {
         event.preventDefault()
+
+        try{
+          await sendRequest(
+              'http://localhost:4003/api/v1/places',
+              'POST',
+              JSON.stringify({
+                  title: formState.inputs.title.value,
+                  description: formState.inputs.description.value,
+                  address: formState.inputs.address.value,
+                  creator: userId
+              }), 
+              { 'Content-Type': 'application/json' }
+          )  
+          navigate('/', { replace: false })
+        } catch(error) {}
+
     }
 
-    return <form className='place-form' onSubmit={placeSubmitHandler}>
-        
+    return <>
+    <ErrorModal error={error} onClear={clearError} />
+    <form className='place-form' onSubmit={placeSubmitHandler}>
+        {isLoading && <LoadingSpinner asOverlay />}
         <Input id='title'
             element='input'
             type='text'
@@ -57,5 +81,6 @@ export default function NewPlace() {
 
         <Button type='submit' disabled={!formState.isValid}>ADD PLACE</Button>
     </form>
+    </>
     
 }
