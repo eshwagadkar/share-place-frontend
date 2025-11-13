0556598e613b2ff148d19ab4e310c53ab1cf05b6
eshwagadkar <eshwagadkar007@gmail.com>
2025-11-13 15:09:46 +0530

    update place - final commit

--- src/places/pages/UpdatePlace.jsx
+++ src/places/pages/UpdatePlace.jsx
@@ -1,44 +1,24 @@
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
+import { useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
 import Input from '../../shared/components/FormElements/Input'
 import Button from '../../shared/components/FormElements/Button'
 import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH } from '../../shared/utils/validators'
 import { useForm } from '../../shared/hooks/form-hooks'
 import Card from '../../shared/components/UI/Card'
-
+import useHttpClient from '../../shared/hooks/http-hooks'
 import './PlaceForm.css'
-
-const DUMMY_PLACES = [
-  {
-    id: 'p1',
-    title: 'Empire State Building',
-    description: 'One of the most famous sky scrapers in the world!',
-    imageURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
-    address: '20 W 34th St, New York, NY 10001',
-    location: {
-      lat: 40.7484405,
-      lng: -73.9878584
-    },
-    creator: 'u1'
-  },
-  {
-    id: 'p2',
-    title: 'Eifel Tower',
-    description: 'One of the most famous sky scrapers in the world',
-    imageURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
-    address: 'Paris',
-    location: {
-      lat: 48.8575475,
-      lng: 2.3513765
-    },
-    creator: 'u2'
-  }
-]
+import LoadingSpinner from '../../shared/components/UI/LoadingSpinner'
+import ErrorModal from '../../shared/components/UI/ErrorModal'
+import { useNavigate } from 'react-router-dom'
 
 export default function UpdatePlace() {
-
+    const [loadedPlace, setLoadedPlace] = useState()
+    const {isLoading, error, sendRequest, clearError} = useHttpClient()
+    const navigate = useNavigate()
+    const userId = useSelector(state => state.auth.userId)
     const placeID = useParams().placeId
-    
+
     const [inputHandler, formState, setFormData] = useForm({
                title: {
                     value: '',
@@ -50,38 +30,56 @@ export default function UpdatePlace() {
                 }
     }, false )
 
-    const identifiedPlace = DUMMY_PLACES.find(place => place.id === placeID)
-
     useEffect(() => {
 
-      if(identifiedPlace){
-        setFormData({
-          title: {
-            value: identifiedPlace.title,
-            isValid: true
-          },
-          description: {
-            value: identifiedPlace.description,
-            isValid: true
-          }
-        }, true)
-      }
+        async function fetchPlace() {
+            try {
+              const responseData = await sendRequest(
+                `http://localhost:4003/api/v1/places/${placeID}`)
+              setLoadedPlace(responseData.place)  
+                setFormData({
+                  title: {
+                    value: responseData.place.title,
+                    isValid: true
+                  },
+                  description: {
+                    value: responseData.place.description,
+                    isValid: true
+                  }
+                 }, true)
+            } catch(error) {}
 
-    }, [setFormData, identifiedPlace])
+        }
 
-    const placeUpdateSubmitHandler = event => {
+        fetchPlace()
+
+     },[sendRequest, placeID, setFormData])
+
+    const placeUpdateSubmitHandler = async event => {
         event.preventDefault()
-    }
 
-    if(!identifiedPlace) {
+        try{
+          await sendRequest(`http://localhost:4003/api/v1/places/${placeID}`, 
+                            'PATCH',
+                            JSON.stringify({
+                              title: formState.inputs.title.value,
+                              description: formState.inputs.description.value
+                            }),
+                            { 'Content-Type': 'application/json' }
+                          )
+          navigate(`/${userId}/places`, { replace: false } )                
+        } catch(error) {}
+    }
+    
+    if(isLoading){ return <div className='center'><LoadingSpinner /></div> }
+
+    if(!loadedPlace && !error) {
       return <div className='center'><Card><h2>Could not find place!</h2></Card></div>
     }
 
-    if(!formState.inputs.title.value){
-       return <div className='center'><h2>Loading...</h2></div>
-    }
-
-    return <form className='place-form' onSubmit={placeUpdateSubmitHandler}>
+    return <>
+       <ErrorModal onClear={clearError} error={error}/>
+      {!isLoading && loadedPlace && <form className='place-form' onSubmit={placeUpdateSubmitHandler}>
         
         <Input 
             id='title'
@@ -91,8 +89,8 @@ export default function UpdatePlace() {
             validators={[VALIDATOR_REQUIRE()]}
             errorText='Please enter a valid title'
             onInput={inputHandler}
-            initialValue={formState.inputs.title.value}
-            initialIsValid={formState.inputs.title.isValid}
+            initialValue={loadedPlace.title}
+            initialIsValid={true}
         />
 
         <Input 
@@ -102,10 +100,11 @@ export default function UpdatePlace() {
             validators={[VALIDATOR_MINLENGTH(5)]}
             errorText='Please enter a valid description (min. 5 characters).'
             onInput={inputHandler}
-            initialValue={formState.inputs.description.value}
-            initialIsValid={formState.inputs.description.isValid}
+            initialValue={loadedPlace.description}
+            initialIsValid={true}
         />
 
         <Button type='submit' disabled={!formState.isValid}>UPDATE PLACE</Button>
-    </form>
+      </form>}
+    </>
 }
