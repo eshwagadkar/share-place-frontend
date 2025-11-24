55c9a5d09cab984f2891c776be7222f5619166fa
eshwagadkar <eshwagadkar007@gmail.com>
2025-11-24 12:39:59 +0530

    update - useServerWarmStatus hook  rollback

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, {Suspense} from 'react'
 import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import LoadingSpinner from './shared/components/UI/LoadingSpinner'
-import { useServerWarmStatus } from './shared/hooks/useServerWarmStatus'
+// import { useServerWarmStatus } from './shared/hooks/useServerWarmStatus'
 
 const RootLayout = React.lazy(() => import('./shared/pages/Root'))
 const Users = React.lazy(() => import('./user/pages/Users'))
@@ -12,20 +12,20 @@ const UpdatePlace = React.lazy(() => import('./places/pages/UpdatePlace'))
 const Auth = React.lazy(() => import('./user/pages/Auth'))
 
 function App() {
-  const backendURL = import.meta.env.VITE_BACKEND_URL
+  // const backendURL = import.meta.env.VITE_BACKEND_URL
   const isLoggedIn = useSelector(state => state.auth.isLoggedIn)
-  const isServerCold = useServerWarmStatus(backendURL)
+  // const isServerCold = useServerWarmStatus(backendURL)
 
-  const fallbackUI = (
-    <div className='center' style={{ textAlign: 'center' }}>
-      <LoadingSpinner asOverlay />
-      {isServerCold && (
-        <p style={{ marginTop: '1rem', color: '#999' }}>
-          Warming up the server… this may take a few seconds
-        </p>
-      )}
-    </div>
-  )
+  // const fallbackUI = (
+  //   <div className='center' style={{ textAlign: 'center' }}>
+  //     <LoadingSpinner asOverlay />
+  //     {isServerCold && (
+  //       <p style={{ marginTop: '1rem', color: '#999' }}>
+  //         Warming up the server… this may take a few seconds
+  //       </p>
+  //     )}
+  //   </div>
+  // )
 
   // Base child routes (always accessible)
   const childRoutes = [
@@ -55,7 +55,7 @@ function App() {
   ])
 
   return (
-    <Suspense fallback={fallbackUI}>
+    <Suspense fallback={<div className='center'><LoadingSpinner asOverlay /></div>}>
       <RouterProvider router={router} />
     </Suspense>
   )
